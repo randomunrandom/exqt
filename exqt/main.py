842e89d301fde12d8cdfa550245df3b4b68f1c8a
Random <randomdanilka@gmail.com>
2018-11-28 12:47:21 +0300

    better beginning

--- exqt/main.py
+++ exqt/main.py
@@ -1,8 +1,8 @@
 import os
-import sys
-import click
+#import sys
 import json
 import subprocess
+import click
 
 
 def get_cmd_file():
@@ -14,75 +14,51 @@ def get_cmd_file():
     return str(os.getcwd() + "/.exqt.json")
 
 
-@click.command()
-@click.argument('name')
-def exqt(name):
-    cmd_lst = get_cmd_file()
-    if cmd_lst == "":
-        print("error, home dir not found")
-        return
-    with open(cmd_lst) as json_data:
-        """if json_data.readline() == "":
+cmd_json = get_cmd_file()
+if cmd_json == "":
+    print("error:\nhome dir not found")
+
+
+def run_script(script):
+    print("script:")
+    print(click.style(script, fg='bright_blue'))
+    print("result:")
+    print(click.style(subprocess.Popen(script, shell=True, stdout=subprocess.PIPE).stdout.read().decode("utf-8"),
+                      fg="yellow"))
+    # if next=="": return
+    return
+
+
+@click.group()
+# @click.option('--verbose', default=False)
+def exqt():
+    # I don't know what to put here
+    return
+
+
+@exqt.command()
+@click.argument('name', nargs=-1)
+def run(name):
+    with open(cmd_json) as json_data:
+        json_lst = json.load(json_data)
+        if json_lst == []:
             print("command not found")
             print("consider using `exqt add` to add new scripts")
-            return"""
-        json_lst = json.load(json_data)
+            return
+        keys_lst = json_lst["scripts"].keys()
+        for i, el in enumerate(name):
+            if el not in keys_lst:
+                print("command", el, "not found")
+                print("consider using `exqt add` to add new scripts")
+            else:
+                script = json_lst["scripts"][el]["script"]
+                run_script(script)
 
-        script = json_lst["scripts"][name]["script"]
-
-        print("script:\n\t", click.style(script, fg='bright_blue'))
-        print("result:")
-        print("\t", click.style(subprocess.Popen(script, shell=True, stdout=subprocess.PIPE).stdout.read().decode("utf-8"),
-                                fg="yellow"))
-        #print(json_lst)
-        #print(json_lst.keys())
+            # print(json_lst)
+            # print(json_lst.keys())
         json_data.close()
 
-    #print(cmd_lst)
-    #print(name)
+    # print(cmd_lst)
+    # print(name)"""
 
-    return
-
-
-def main():
-    exqt()
-    return
-    """
-    user_home = os.path.expanduser("~")
-
-    if argv is None:
-        argv = sys.argv[1:]
-
-    parser = argparse.ArgumentParser(
-        prog="exqt",
-        description="cli program to save and execute difficult/long commands on demand"
-    )
-    parser.add_argument("command_name", help="execute command saved by given name", type=str)
-    arguments = parser.parse_args(argv)
-    prog_name = arguments.command_name
-    if ".exqt" not in os.listdir(user_home):
-        with open(f"{user_home}/.exqt", 'w', encoding='utf-8') as f:
-            json.dump({
-                "commands": {
-                    "echo_test": {
-                        "name": "echo_test",
-                        "script": 'echo "hello world"'
-                    },
-                    "ping_google": {
-                        "name": "ping_google",
-                        "script": 'ping -c 5 google.com'
-                    }
-                }
-            }, fp=f, ensure_ascii=False)
-
-    with open(f"{user_home}/.exqt", 'r', encoding='utf-8') as f:
-        commands = json.loads(f.read())
-
-    prog_script = commands['commands'][prog_name]['script']
-    print("- script:", file=fp)
-    print(colored(prog_script, "green"), file=fp)
-    print("- output:", file=fp)
-    print(colored(subprocess.Popen(prog_script, shell=True, stdout=subprocess.PIPE).stdout.read().decode("utf-8"),
-                  "yellow"), file=fp)
-    """
     return
